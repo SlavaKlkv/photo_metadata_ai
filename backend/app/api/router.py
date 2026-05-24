a8b53ee39880e52c63da3e687459ff7130d74561
Slava Kulikov <slava_kulikov@outlook.com>
2026-05-24 18:29:38 +0300

    refactor: reorganize job endpoints

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.jobs import router as jobs_router
+from app.api.v1.jobs.jobs import router as jobs_router
+from app.api.v1.jobs.internal import router as internal_router
 from app.core.config import settings
 
 router_v1 = APIRouter(prefix=settings.API_V1_STR)
 router_v1.include_router(jobs_router)
+router_v1.include_router(internal_router)
